eccef3a67e917a99f8b0a0b461b8eef5374bcc74
ChoonMeng <chanchoonmeng202@gmail.com>
2026-02-24 18:09:46 +0800

    initial setup for backend

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+
+app.use('/', (req, res) => {
+    res.send('Hello, Game Store Backend!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
